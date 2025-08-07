1890c8491c64739fe37f4dc1c1c2b89dfdf425a1
JawadElhassan <jawadnelhassan2005@hotmail.com>
2025-08-07 10:25:11 -0500

    Update scatterplot.py

--- scatterplot.py
+++ scatterplot.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load CSV
-df = pd.read_csv("mini3di_outputs_structural.csv")
+df = pd.read_csv("CASP15.csv")
 
 # Drop attempts with missing or zero predictions
 df = df[df["PredictedLen"] > 0]
